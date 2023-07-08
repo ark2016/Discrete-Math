ff8b3362094a8baa498e3af40e05132df30b3443
Arkadiy <91221701+ark2016@users.noreply.github.com>
2023-07-08 13:23:46 +0300

    Update qsort.go
    fix rec

--- qsort.go
+++ qsort.go
@@ -1,48 +1,33 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
-
-var (
-	arr [100]int
-)
-
-func less(i, j int) bool {
-	return arr[i] <= arr[j]
-}
-func swap(i, j int) {
-	arr[i], arr[j] = arr[j], arr[i]
-}
-func partition(swap func(i, j int), less func(i, j int) bool, low int, high int) int {
+func partition(low int, high int, less func(i int, j int) bool, swap func(i int, j int)) int {
 	i := low
 	for j := low; j < high; j++ {
 		if less(j, high) {
-			swap(j, i)
+			swap(i, j)
 			i++
 		}
 	}
 	swap(high, i)
 	return i
 }
-func quickSortRec(swap func(i, j int), less func(i, j int) bool, low int, high int) {
-	var q int
-	if low < high {
-		q = partition(swap, less, low, high)
-		quickSortRec(swap, less, low, q-1)
-		quickSortRec(swap, less, q, high)
-	}
 
-}
-func qsort(n int, less func(i, j int) bool, swap func(i, j int)) {
-	quickSortRec(swap, less, 0, n-1)
-}
-func main() {
-	for i := 0; i < 100; i++ {
-		arr[i] = rand.Intn(100)
+func quickSortRec(low int, high int, less func(i int, j int) bool, swap func(i int, j int)) {
+	if low < high {
+		q := partition(low, high, less, swap)
+		if low < q-1 {
+			quickSortRec(low, q-1, less, swap)
+		}
+		if high > q+1 {
+			quickSortRec(q+1, high, less, swap)
+		}
 	}
-	fmt.Println(arr)
-	qsort(100, less, swap)
-	fmt.Println(arr)
+}
+
+func qsort(n int, less func(i, j int) bool, swap func(i, j int)) {
+	quickSortRec(0, n-1, less, swap)
+}
+
+func main() {
+
 }
